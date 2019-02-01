d654829bbf328e7d9d1a0ea16120118fb3275612
chinge55 <33897366+chinge55@users.noreply.github.com>
2019-02-01 10:31:57 -0500

    Initial commit...

---
+++ PortScn.java
@@ -0,0 +1,44 @@
+import java.util.concurrent.atomic.AtomicInteger;
+import java.net.*;
+class Counter
+{
+	AtomicInteger count = new AtomicInteger(0);
+	public void increment()
+	{
+		count.incrementAndGet();
+	}
+}
+public class PortScn
+{
+	public static void main(String[] args)
+	{
+		Counter c = new Counter();
+		// total posts scanned = 50* 1000
+		for(int i=0; i<50; i++)
+		{
+			Thread temp = new Thread(new Runnable()
+			{	public void run()
+				{
+					Socket s;
+					
+					for(int i=0; i<1000; i++)
+					{
+						try
+						{	int port = c.count.get();	
+							c.increment();
+							// specify ip address you want to scan
+							s = new Socket("127.0.0.1",port);
+							System.out.println("Port open:"+c.count);
+						}catch(Exception e){
+						}
+						
+					}	
+				
+				}
+			});
+			temp.run();
+	
+		}	
+	System.out.println("Count "+c.count); 	
+}
+}
